30c29cda9a7b67c857140f839dd21ace88377f4e
Denis Moraes Rêgo Campos <denismrc8@gmail.com>
2017-04-29 17:40:21 -0300

    colocando modal nos pedidos encerrados

--- public/js/app.js
+++ public/js/app.js
@@ -55,3 +55,19 @@ function alteraFormPedido(tipo){
         $('#div-cliente_id').show('slow');
     }
 }
+
+
+function popupcliente(Nome,Tipo,Contato){
+    var tipopessoa;
+    if(Tipo==1){
+        tipopessoa = "Fisico";
+    }else{
+        tipopessoa = "Jurídica";
+    }
+    $("#Titulomodal").html(Nome);
+    $("#Corpomodal").html(
+        "Tipo: "+tipopessoa+"<br>Contato: "+Contato
+    );
+    $("#salvemsg").modal();
+}
+
